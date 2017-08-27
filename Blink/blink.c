8c1fd1fd60e4aef858c0cde22da981ade7af633c
luquesada7 <lu.quesada7@gmail.com>
2017-08-26 18:04:43 -0600

    Blink

---
+++ Blink/blink.c
@@ -0,0 +1,85 @@
+/******************************************************************************
+*
+* MSP432 blink.c - P1.0 port toggle
+*
+* CMSIS compliant coding
+*
+******************************************************************************/
+
+#include "msp.h"
+#include "blink.h"
+
+
+// ***********************************
+// Main loop
+// ***********************************
+volatile uint16_t	g_TimerCount =0;
+void main(void)
+{
+    Setup();
+   while(1)
+   {
+	   // - Wait for events
+	   __wfe();
+   }
+}
+
+// **********************************
+// Interrupt service routine for
+// Timer A0
+// **********************************
+void TA0_0_ISR(void)
+{
+	// - Divide the clock further, to achieve human readable times.
+	if(g_TimerCount == TIMERA0_COUNT)
+	{
+		// - Toggle P1.0
+		P1->OUT ^= BIT0;
+		g_TimerCount = 0U;
+
+	}
+	else
+	{
+		g_TimerCount+=1;
+	}
+	return;
+}
+
+// **********************************
+// Setup function for the application
+// @input - none
+// @output - none
+// **********************************
+void Setup(void)
+{
+	// ****************************
+	//         DEVICE CONFIG
+	// ****************************
+	// - Disable WDT
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;
+
+
+	// ****************************
+	//         PORT CONFIG
+	// ****************************
+	// - P1.0 is connected to the Red LED
+	P1->DIR |= BIT0;
+
+	// ****************************
+	//       TIMER CONFIG
+	// ****************************
+	// - Disable all interrupts
+	// - Configure Timer A0 with SMCLK, Division by 8, Enable the interrupt
+	// - Enable the interrupt in the NVIC
+	// - Start the timer in UP mode.
+	// - Re-enable interrupts
+	__disable_irq();
+	TIMER_A0->CTL = TIMER_A_CTL_SSEL__SMCLK | TIMER_A_CTL_ID__2 | TIMER_A_CTL_IE;
+	NVIC_SetPriority(TA0_N_IRQn,1);
+	NVIC_EnableIRQ(TA0_N_IRQn);
+	TIMER_A0->CCR[0] = 0xFFFF;
+	TIMER_A0->CTL |=  TIMER_A_CTL_MC__UP;
+	__enable_irq();
+
+	return;
+}
---
+++ Blink/blink.h
@@ -0,0 +1,16 @@
+/*
+ * blink.h
+ *
+ *  Created on: Aug 17, 2016
+ *      Author: eortiz
+ */
+
+#ifndef BLINK_H_
+#define BLINK_H_
+
+#define NO_ERR 0
+#define TIMERA0_COUNT 16000
+
+void Setup(void);
+
+#endif /* BLINK_H_ */
